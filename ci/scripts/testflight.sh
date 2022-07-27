dfc5efda0d87a43e2bdb695ffa6248dc577e9064
Ioannis Tsouvalas <tsouvalasi@atic.gr>
2022-07-27 12:06:49 +0000

    populating vault secrets

--- ci/scripts/testflight.sh
+++ ci/scripts/testflight.sh
@@ -14,5 +14,11 @@ cd dev-changes
 pwd
 ls -la
 
+header "Adding required safe targets"
+echo "Vault Address used: $VAULT_ADDR"
+safe target vault "$VAULT_ADDR" -k
+export VAULT_ROLE_ID=$VAULT_ROLE
+export VAULT_SECRET_ID=$VAULT_SECRET
+
 header "Testing addons..."
 genesis do dev list
