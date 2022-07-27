edc7d183f6de78542f13f1f128e62e442c79fca0
Ioannis Tsouvalas <tsouvalasi@atic.gr>
2022-07-27 12:23:19 +0000

    relying on vars

--- ci/scripts/testflight.sh
+++ ci/scripts/testflight.sh
@@ -10,15 +10,15 @@ header() {
 }
 
 header "Changing to dev-changes directory"
-cd dev-changes
+cd $GIT_DIRECTORY
 pwd
 ls -la
 
 header "Adding required safe targets"
-echo "Vault Address used: $VAULT_ADDR"
+
 safe target vault "$VAULT_ADDR" -k
 export VAULT_ROLE_ID=$VAULT_ROLE
 export VAULT_SECRET_ID=$VAULT_SECRET
 
-header "Testing addons..."
-genesis do dev list
+header "Listing" addons..."
+genesis do $BOSH_DIRECTOR list
