ed0b6f935803e3c2a1935d4aa4f615e5207cf4ed
Ioannis Tsouvalas <tsouvalasi@atic.gr>
2022-08-01 11:01:02 +0000

    check for and update or bind as

--- ci/scripts/testflight.sh
+++ ci/scripts/testflight.sh
@@ -32,7 +32,20 @@ export CF_USERNAME=$CF_USERNAME
 export CF_PASSWORD=$CF_PASSWORD
 cf auth
 
-header "Testing addons..."
+header "Testing autoscaler binding..."
 genesis do $BOSH_DIRECTOR -- setup-cf-plugin -f
+
+# Identify the presence of autoscaler service broker and either update or bind it
+autoscaler_registered=$(cf curl /v2/service_brokers|jq --arg env_name "autoscaler" -r '.resources[].entity | select(.name==$env_name) | .name')
+if [[ -n $autoscaler_registered ]] ; then
+
+	describe "Found and updating service broker autoscaler..."
+	genesis do $BOSH_DIRECTOR -- update-autoscaler
+
+else
+
+	describe "Creating service broker autoscaler..."
+	genesis do $BOSH_DIRECTOR -- bind-autoscaler
+
+# Test overall binding process
 genesis do $BOSH_DIRECTOR -- test-bind-autoscaler
-genesis do $BOSH_DIRECTOR -- update-autoscaler
